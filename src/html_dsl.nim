d6a8829fd6ff7cbe0857fdafdf89d112ca7a5bd3
juancarlospaco <juancarlospaco@gmail.com>
2020-01-06 15:14:54 -0300

    clean

--- src/html_dsl.nim
+++ src/html_dsl.nim
@@ -316,104 +316,77 @@ func newa*(href, val: string, rel="", id="", class=""): HtmlNode =
 
 
 
-func link*(href: string, hreflang="", crossorigin="", integrity="", media="", referrerpolicy="", sizes=""): HtmlNode =
-  ## Create a new ``<link>`` tag Node with attributes. No children.
-  HtmlNode(kind: nkLink, href: href, crossorigin: crossorigin, integrity: integrity,
-           media: media, referrerpolicy: referrerpolicy, sizes: sizes, hreflang: hreflang)
 
-proc meta*(name, content: string, httpequiv=""): HtmlNode =
-  ## Create a new ``<meta>`` tag Node with name,content,httpequiv. No children.
-  echo conta
-  result = HtmlNode(kind: nkMeta, name: name, content: content, httpequiv: httpequiv)
+func meta*(name, content: string, httpequiv=""): HtmlNode {.inline.} = HtmlNode(kind: nkMeta, name: name, content: content, httpequiv: httpequiv) ## Create a new ``<meta>`` tag Node with name,content,httpequiv. No children.
+func title*(titulo: string): HtmlNode {.inline.} = HtmlNode(kind: nkTitle, text: titulo.strip.capitalizeAscii) ## Create a new ``<title>`` tag Node with text string, title is Capitalized.
+func link*(href: string, hreflang="", crossorigin="", integrity="", media="", referrerpolicy="", sizes=""): HtmlNode {.inline.} =
+  HtmlNode(kind: nkLink, href: href, crossorigin: crossorigin, integrity: integrity, media: media, referrerpolicy: referrerpolicy, sizes: sizes, hreflang: hreflang)
 
-
-
-
-
-func newHead*(title: HtmlNode, meta: varargs[HtmlNode], link: varargs[HtmlNode]): HtmlNode =
-  ## Create a new ``<head>`` tag Node with meta, link and title tag nodes.
-  HtmlNode(kind: nkHead, title: title, meta: @meta, link: @link)
+func newHead*(title: HtmlNode, meta: varargs[HtmlNode], link: varargs[HtmlNode]): HtmlNode {.inline.} = HtmlNode(kind: nkHead, title: title, meta: @meta, link: @link) ## Create a new ``<head>`` tag Node with meta, link and title tag nodes.
 
 macro head*(inner: untyped): HtmlNode =
   ## Macro to call ``newHead()`` with the childrens.
   assert inner.len >= 1, "Head Error: Wrong number of inner tags:" & $inner.len
   result = newCall("newHead")
-  if inner.len == 1: result.add(inner)
+  if inner.len == 1: result.add inner
   inner.copyChildrenTo(result)
 
-func title*(titulo: string): HtmlNode =
-  ## Create a new ``<title>`` tag Node with text string, title is Capitalized.
-  HtmlNode(kind: nkTitle, text: titulo.strip.capitalizeAscii) # Only title arg
-
-func newBody*(children: varargs[HtmlNode]): HtmlNode =
-  ## Create a new ``<body>`` tag Node, containing all children tags.
-  HtmlNode(kind: nkBody, children: @children) # Body have childrens.
+func newBody(children: varargs[HtmlNode]): HtmlNode {.inline.} = HtmlNode(kind: nkBody, children: @children) ## Create a new ``<body>`` tag Node, containing all children tags.
 
 macro body*(inner: untyped): HtmlNode =
   ## Macro to call ``newBody()`` with the childrens, if any.
   assert inner.len >= 1, "Body Error: Wrong number of inner tags:" & $inner.len
   result = newCall("newBody") # Result is a call to newBody()
-  if inner.len == 1: result.add(inner) # if just 1 children just pass it as arg
+  if inner.len == 1: result.add inner # if just 1 children just pass it as arg
   inner.copyChildrenTo(result) # if several children copy them all, AST level.
 
-proc newHtml*(head, body: HtmlNode): HtmlNode =
-  ## Create a new ``<html>`` tag Node, containing a ``<head>`` and ``<body>``.
-  HtmlNode(kind: nkHtml, head: head, body: body) # Head & Body have childrens.
+func newHtml(head, body: HtmlNode): HtmlNode {.inline.} = HtmlNode(kind: nkHtml, head: head, body: body)
 
 macro html*(name: untyped, inner: untyped) =
-  ## Macro to create a new call to ``newHtml()``, passing Head and Body as arg.
-  var rs = newCall("newHtml", inner[0], inner[1])  # Call newHtml(head, body)
+  let rs = newCall("newHtml", inner[0], inner[1])  # Call newHtml(head, body)
   result = quote do: # inner is the whole content of the HTML DSL (head + body)
     proc `name`(): HtmlNode {.inline.} = `rs` # Do name() = newHtml(head, body)
 
-template indent_if_needed(thingy, indentationLevel: untyped): untyped =
-  ## Render Pretty-Printed with indentation when build for Release,else Minified
-  when defined(release): thingy           # Release, Minified for Performance.
-  else: indent(thingy, indentationLevel) # Development, use the Indentations.
+template indentIfNeeded(thingy, indentationLevel: untyped): untyped =
+  when defined(release): thingy else: indent(thingy, indentationLevel)
 
-proc render*(this: HtmlNode): string =
+func render*(this: HtmlNode): string =
   ## Render HtmlNode with indentation return string.
   var indentationLevel: byte   # indent level, 0 ~ 255.
   case this.kind
   of nkHtml:                    # <html>
-    result &= open_tag this
+    result &= openTag this
     inc indentationLevel
-    result &= indent_if_needed(render(this.head), indentationLevel)
-    result &= indent_if_needed(render(this.body), indentationLevel)
+    result &= indentIfNeeded(render(this.head), indentationLevel)
+    result &= indentIfNeeded(render(this.body), indentationLevel)
     dec indentationLevel
     result &= close_tag this
   of nkHead:                    # <head>
-    result &= open_tag this
+    result &= openTag this
     inc indentationLevel
     if this.meta.len > 0:
-      for meta_tag in this.meta:  # <meta ... >
-        result &= indent_if_needed(render(meta_tag), indentationLevel)
+      for meta_tag in this.meta: result &= indentIfNeeded(render(meta_tag), indentationLevel)  # <meta ... >
     if this.link.len > 0:
-      for link_tag in this.link:  # <link ... >
-        result &= indent_if_needed(render(link_tag), indentationLevel)
-    result &= indent_if_needed(open_tag(this.title), indentationLevel)
+      for link_tag in this.link: result &= indentIfNeeded(render(link_tag), indentationLevel) # <link ... >
+    result &= indentIfNeeded(openTag(this.title), indentationLevel)
     dec indentationLevel
     result &= close_tag this
   of nkBody:                    # <body>
-    result &= open_tag this
+    result &= openTag this
     inc indentationLevel
     if this.children.len > 0:
       for tag in this.children:
-        if tag.kind in canHaveChildren:
-          result &= indent_if_needed(render(tag), indentationLevel)
-        else:
-          result &= indent_if_needed(open_tag(tag), indentationLevel)
+        if tag.kind in canHaveChildren: result &= indentIfNeeded(render(tag), indentationLevel)
+        else: result &= indentIfNeeded(openTag(tag), indentationLevel)
     dec indentationLevel
     result &= close_tag this
   else:
-    result &= open_tag this
+    result &= openTag this
     inc indentationLevel
     if this.children.len > 0:
       for tag in this.children:
-        if tag.kind in canHaveChildren:
-          result &= indent_if_needed(render(tag), indentationLevel)
-        else:
-          result &= indent_if_needed(open_tag(tag), indentationLevel)
+        if tag.kind in canHaveChildren: result &= indentIfNeeded(render(tag), indentationLevel)
+        else: result &= indentIfNeeded(openTag(tag), indentationLevel)
     dec indentationLevel
     result &= close_tag this
 
@@ -428,14 +401,14 @@ proc render*(this: HtmlNode): string =
   #    nkSection, nkSelect, nkSmall, nkSource, nkSpan, nkStrong, nkSub, nkSummary,
   #    nkSup, nkTable, nkTbody, nkTd, nkTemplate, nkTfoot, nkTh, nkThead, nkTr,
   #    nkTrack, nkTt, nkU, nkUl:  # All other tags
-  #   result &= open_tag this
+  #   result &= openTag this
   #   inc indentationLevel
   #   if this.children.len > 0:
   #     for tag in this.children:
   #       if tag.kind in canHaveChildren:
-  #         result &= indent_if_needed(render(tag), indentationLevel)
+  #         result &= indentIfNeeded(render(tag), indentationLevel)
   #       else:
-  #         result &= indent_if_needed(open_tag(tag), indentationLevel)
+  #         result &= indentIfNeeded(openTag(tag), indentationLevel)
   #   dec indentationLevel
   #   result &= close_tag this
   # else:
